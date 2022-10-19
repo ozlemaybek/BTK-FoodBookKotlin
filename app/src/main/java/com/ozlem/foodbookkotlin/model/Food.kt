74ab1d8a6aaf977d84f744cd11cf0dbbee29a296
Özlem Katibe AYBEK <ozlemkatibeaybek@gmail.com>
2022-10-19 12:28:12 +0300

    Project updated.

--- app/src/main/java/com/ozlem/foodbookkotlin/model/Food.kt
+++ app/src/main/java/com/ozlem/foodbookkotlin/model/Food.kt
@@ -1,22 +1,41 @@
 package com.ozlem.foodbookkotlin.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 // String nullable yapmamızın nedeni: çektiğimiz verilerde bazı bölümler boş olabilir.
 // Verimizden gelecek isimlerle tamamen farklı isimleri seçtik.
 // Farklı isimler seçtiğimizde birçok işlem kolaylaşıyor fakat bu kullanım şeklini görmek istedik:
+
+@Entity
 data class Food (
     // Neye göre serileştirileceğini söylüyoruz:
+    // ColumnInfo ile bir alt satırdakinin kolonlarımızdan biri olacağını söylüyoruz.
+    @ColumnInfo(name = "isim")
     @SerializedName("isim")
     val name : String? ,
+    @ColumnInfo(name = "kalori")
     @SerializedName("kalori")
     val calorie : String? ,
+    @ColumnInfo(name = "karbonhidrat")
     @SerializedName("karbonhidrat")
     val carbohydrate : String? ,
+    @ColumnInfo(name = "protein")
     @SerializedName("protein")
     val protein : String? ,
-    @SerializedName("yağ")
+    @ColumnInfo(name = "yag")
+    @SerializedName("yag")
     val oil : String? ,
+    @ColumnInfo(name = "gorsel")
     @SerializedName("gorsel")
-    val image : String? ){
+    val image : String?
+    ){
+
+    // Yukarıdaki bilgileri primary constructor'a ekledik. Fakat primary key'i class'ın bosy'sine ekleyeceğiz:
+    // oluşturduğumuz primary key'i uuid içine atadık.
+    // Artık primary key otomatik olarak oluşturulacak ve eklediğimiz verilerin içerisine id olrak atanacak.
+    @PrimaryKey(autoGenerate = true)
+    var uuid : Int = 0
 }
